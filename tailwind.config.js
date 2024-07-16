07572d1e577ce84e7c0aace32d6d6e8d8440d7d9
Rokaya Nasr <rokanasr60@gmail.com>
2024-07-16 15:55:08 +0300

    ,

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
-  theme: {
-    extend: {},
-    colors: {
-      primary: "#301d48",
-      secondary: "#e6d0e7",
-      secondarybg: "#F5F2F7",
-      greeny: "#94cbc4",
-      yellowy: "#fffad3",
-      grey1 : "#D1D1DC",
-      grey2: "#EFF3FF"
-  },
-  },
-  plugins: [require("flowbite/plugin")],
-}
-
+    content: ["./node_modules/flowbite/**/*.js", "./src/**/*.{html,js}"],
+    theme: {
+        extend: {},
+        colors: {
+            primary: "#301d48",
+            secondary: "#e6d0e7",
+            secondarybg: "#F5F2F7",
+            greeny: "#94cbc4",
+            yellowy: "#fffad3",
+            grey1: "#D1D1DC",
+            grey2: "#EFF3FF",
+        },
+    },
+    plugins: [require("flowbite/plugin")],
+};
